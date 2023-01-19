2ebef5f6b54b76ba2c0a3b5c4641d4373db0af56
unknown <brenomoraes97@yahoo.com>
2023-01-19 18:48:50 -0300

    feat: created routes post/habits and get/day

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,11 @@
 import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
 import cors from "@fastify/cors";
+import { appRoutes } from "./lib/routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/", async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: "beber",
-      },
-    },
-  });
-
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
