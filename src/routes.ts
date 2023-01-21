43e15f9ee863326b1cbd09041a51047030c3ba7c
unknown <brenomoraes97@yahoo.com>
2023-01-21 12:54:52 -0300

    feat: created route summary

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
-import { prisma } from "./prisma";
+import { prisma } from "./lib/prisma";
 
 export const appRoutes = async (app: FastifyInstance) => {
   app.post("/habits", async (request) => {
@@ -117,5 +117,31 @@ export const appRoutes = async (app: FastifyInstance) => {
         },
       });
     }
+  });
+
+  app.get("/summary", async () => {
+    const summary = await prisma.$queryRaw`
+    SELECT 
+      D.id,
+      D.date,
+      (
+        SELECT 
+          cast(count(*) as float)
+        FROM day_habits DH
+        WHERE DH.day_id = D.id
+      ) as completed,
+      (
+        SELECT 
+        cast(count(*) as float)
+        FROM habit_week_days HWD
+        JOIN habits H
+        ON H.id = HWD.habit_id
+        WHERE 
+        HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+        AND H.created_at <= D.date
+      ) as amount
+    FROM days D
+    `;
+    return summary;
   });
 };
